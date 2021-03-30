4204146c7f0d39a426ae0a36f8e87858368505d8
Norne9 <norne19@outlook.com>
2021-03-30 16:14:59 +0500

    All calculator files

--- Calculator/Calculator/CalculatorModel.swift
+++ Calculator/Calculator/CalculatorModel.swift
@@ -6,3 +6,102 @@
 //
 
 import Foundation
+
+enum CalculatorAction {
+    case plus
+    case minus
+    case divide
+    case multiply
+    case clear
+    case sign
+    case percent
+    case run
+}
+
+class Calculator {
+    var numberOnScreen: Double = 0
+    var numberInMemory: Double = 0
+    var tempNumber: String = "0"
+    var startedTyping: Bool = false
+    var selectedActon: CalculatorAction = .clear
+    
+    let updateScreen: (String) -> ()
+    
+    
+    init(onUpdate updateScreen: @escaping (String) -> ()) {
+        self.updateScreen = updateScreen
+    }
+    
+    func showResult() {
+        startedTyping = false
+        updateScreen(numToString(num: numberOnScreen))
+    }
+    
+    func numToString(num: Double) -> String {
+        String(num)
+    }
+    
+    func sendChar(char mbChar: Character?) {
+        let whiteList = "0123456789."
+        guard let char = mbChar else {
+            return
+        }
+        if !whiteList.contains(char) {
+            return
+        }
+        
+        if !startedTyping {
+            tempNumber = ""
+        }
+        startedTyping = true
+        tempNumber += String(char)
+        numberOnScreen = Double(tempNumber) ?? 0.0
+        updateScreen(tempNumber)
+    }
+    
+    func sendAction(action: CalculatorAction) {
+        switch action {
+        case .plus, .minus, .divide, .multiply:
+            selectedActon = action
+            numberInMemory = numberOnScreen
+            showResult()
+            return
+        default:
+            break
+        }
+        
+        switch action {
+        case .clear:
+            numberOnScreen = 0.0
+            numberInMemory = 0.0
+            showResult()
+        case .sign:
+            numberOnScreen = -numberOnScreen
+            tempNumber = numToString(num: numberOnScreen)
+            updateScreen(tempNumber)
+        case .percent:
+            numberOnScreen /= 100.0
+            tempNumber = numToString(num: numberOnScreen)
+            updateScreen(tempNumber)
+        case .run:
+            switch selectedActon {
+            case .plus:
+                numberInMemory += numberOnScreen
+            case .minus:
+                numberInMemory -= numberOnScreen
+            case .divide:
+                numberInMemory /= numberOnScreen
+            case .multiply:
+                numberInMemory *= numberOnScreen
+            default:
+                print("Wrong arithmetic action: \(action)")
+                break
+            }
+            numberOnScreen = numberInMemory
+            showResult()
+        default:
+            print("Wrong action: \(action)")
+            break
+        }
+    }
+}
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var model: Calculator!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        model = Calculator(onUpdate: showComputedResult)
     }
 
+    func showComputedResult(result: String) {
+        resultLabel.text = result
+    }
 
+    @IBAction func onNumberPressed(_ sender: UIButton) {
+        model.sendChar(char: sender.currentTitle?.first ?? "?")
+    }
+    
+    @IBAction func onActionPressed(_ sender: UIButton) {
+        switch sender.currentTitle {
+        case "AC":
+            model.sendAction(action: .clear)
+        case "+/-":
+            model.sendAction(action: .sign)
+        case "%":
+            model.sendAction(action: .percent)
+        case "/":
+            model.sendAction(action: .divide)
+        case "*":
+            model.sendAction(action: .multiply)
+        case "+":
+            model.sendAction(action: .plus)
+        case "-":
+            model.sendAction(action: .minus)
+        case "=":
+            model.sendAction(action: .run)
+        default:
+            print("Unknown action: \(sender.currentTitle ?? "Unknown")")
+        }
+    }
 }
 
