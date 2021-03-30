4f96e14883c0c47a92605be4f8f90439847678e2
Norne9 <norne19@outlook.com>
2021-03-30 21:24:43 +0500

    Xylophone works

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var notePlayers: [String: AVAudioPlayer] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadSounds()
+    }
+    
+    func loadSounds() {
+        // Set audio category to playback
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+        } catch let error {
+            print("Failed to set category: \(error.localizedDescription)")
+        }
+        
+        // Load sounds
+        let sounds = "ABCDEFG"
+        for csound in sounds {
+            // Char to string
+            let sound = String(csound)
+            // Try to find path
+            guard let url = Bundle.main.url(forResource: sound, withExtension: "wav", subdirectory: "Sounds") else {
+                print("File for \(sound) sound not found!")
+                continue
+            }
+            // Try to load
+            do {
+                let player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+                player.prepareToPlay()
+                notePlayers[sound] = player
+            } catch let error {
+                print("Error while loading \(sound): \(error.localizedDescription)")
+            }
+        }
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let sound = sender.currentTitle ?? "Unknown"
+        if let player = notePlayers[sound] {
+            player.currentTime = 0.0;
+            player.play()
+        } else {
+            print("Player for \(sound) not found!")
+        }
+    }
+    
 }
 
