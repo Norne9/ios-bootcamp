abcc25dcee6d5ad0d32eef00a7f37f03d0899c36
Norne9 <norne19@outlook.com>
2021-03-31 21:32:15 +0500

    EggTimer - counting works

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -9,24 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let times = [
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let boilTimes = [
         "soft": 5,
-        "medium": 7,
-        "hard": 12
+        "medium": 7 * 60,
+        "hard": 12 * 60
     ]
+    weak var boilTimer: Timer? = nil
+    var currentTime = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        guard let cookTime = times[sender.currentTitle ?? "nil"] else {
+        guard let boilTime = boilTimes[sender.currentTitle ?? "nil"] else {
             print("Unknown hardness: \(sender.currentTitle ?? "nil")")
             return
         }
-        print(cookTime)
+        print(boilTime)
+        
+        boilTimer?.invalidate()
+        currentTime = boilTime
+        boilTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: timerTick)
+        boilTimer?.fire()
+    }
+    
+    func timerTick(timer: Timer) {
+        currentTime -= 1
+        if currentTime <= 0 {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+        } else {
+            titleLabel.text = "Wait \(currentTime) seconds"
+        }
     }
 }
 
