71aab813b5aecf32648b15adc79324c287f6ad4a
Norne9 <norne19@outlook.com>
2021-04-05 14:52:24 +0500

    Photo Processor Created

---
+++ PhotoProcessor/PhotoProcessor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoProcessor
+//
+//  Created by Aleksey Pestov on 05.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
