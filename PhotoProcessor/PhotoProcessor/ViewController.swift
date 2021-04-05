7f8ff80538c1930d2a07337a7be3fd6f04a8e528
Norne9 <norne19@outlook.com>
2021-04-05 16:54:33 +0500

    Photo processor WIP

--- PhotoProcessor/PhotoProcessor/ViewController.swift
+++ PhotoProcessor/PhotoProcessor/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var filtersView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        filtersView.alpha = 0.8
+        addFilterButtons(names: (1...5).map {"Filter \($0)"})
     }
 
-
+    func addFilterButtons(names: [String]) {
+        for name in names {
+            let button = UIButton(type: .system)
+            button.setTitle(name, for: .normal)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            filtersView.addArrangedSubview(button)
+        }
+        filtersView.layoutIfNeeded()
+    }
+    
+    @IBAction func toggleFilters(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5) {
+            if self.filtersView.isHidden {
+                self.filtersView.isHidden = false
+                sender.isSelected = true
+            } else {
+                self.filtersView.isHidden = true
+                sender.isSelected = false
+            }
+        }
+    }
 }
 
