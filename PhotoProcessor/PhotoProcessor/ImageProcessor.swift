d10a9f6805193eb8621edcd193eebc34e8212eb3
Norne9 <norne19@outlook.com>
2021-04-06 15:24:07 +0500

    Photo processor works

---
+++ PhotoProcessor/PhotoProcessor/ImageProcessor.swift
@@ -0,0 +1,73 @@
+//
+//  Filters.swift
+//  PhotoProcessor
+//
+//  Created by Aleksey Pestov on 06.04.2021.
+//
+
+import Foundation
+import UIKit
+import CoreImage
+
+enum FilterError: Error {
+    case noImage
+    case ciFailed
+    case unknowFilter(String)
+    case processingError
+}
+
+class ImageProcessor {
+    let context: CIContext
+    let filters = [
+        "Sepia": applySepia,
+        "Bloom": applyBloom,
+        "Blur": applyBlur,
+    ]
+    var knownFilters: [String] {
+        [String](filters.keys)
+    }
+    
+    init() {
+        context = CIContext()
+    }
+    
+    func applyFilter(_ filter: String?, to uiImage: UIImage?, withPower power: Double) throws -> UIImage {
+        guard let uiImage = uiImage else {
+            throw FilterError.noImage
+        }
+        guard let image = uiImage.ciImage ?? CIImage(image: uiImage) else {
+            throw FilterError.ciFailed
+        }
+        guard let filter = filter else {
+            throw FilterError.unknowFilter("none")
+        }
+        guard let filterFunc = filters[filter] else {
+            throw FilterError.unknowFilter(filter)
+        }
+        guard let newImage = filterFunc(image, power) else {
+            throw FilterError.processingError
+        }
+        return UIImage(ciImage: newImage)
+    }
+    
+    static func applySepia(image: CIImage, power: Double) -> CIImage? {
+        let sepiaFilter = CIFilter(name:"CISepiaTone")
+        sepiaFilter?.setValue(image, forKey: kCIInputImageKey)
+        sepiaFilter?.setValue(power, forKey: kCIInputIntensityKey)
+        return sepiaFilter?.outputImage
+    }
+    static func applyBlur(image: CIImage, power: Double) -> CIImage? {
+        let sepiaFilter = CIFilter(name:"CIGaussianBlur")
+        sepiaFilter?.setValue(image, forKey: kCIInputImageKey)
+        sepiaFilter?.setValue(power * 30.0, forKey: kCIInputRadiusKey)
+        return sepiaFilter?.outputImage
+    }
+    static func applyBloom(image: CIImage, power: Double) -> CIImage?
+    {
+        let bloomFilter = CIFilter(name:"CIBloom")
+        bloomFilter?.setValue(image, forKey: kCIInputImageKey)
+        bloomFilter?.setValue(power, forKey: kCIInputIntensityKey)
+        bloomFilter?.setValue(10.0, forKey: kCIInputRadiusKey)
+        return bloomFilter?.outputImage
+    }
+}
--- PhotoProcessor/PhotoProcessor/ViewController.swift
+++ PhotoProcessor/PhotoProcessor/ViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var filtersView: UIStackView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imageProcessor = ImageProcessor()
+    var originalImage: UIImage?
+    var processedImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        originalImage = imageView.image
+        processedImage = originalImage
+        
         filtersView.alpha = 0.8
-        addFilterButtons(names: (1...5).map {"Filter \($0)"})
+        addFilterButtons(names: imageProcessor.knownFilters)
     }
 
     func addFilterButtons(names: [String]) {
@@ -22,9 +30,27 @@ class ViewController: UIViewController {
             let button = UIButton(type: .system)
             button.setTitle(name, for: .normal)
             button.translatesAutoresizingMaskIntoConstraints = false
+            button.addTarget(self, action: #selector(filterPressed(sender:)), for: .touchUpInside)
             filtersView.addArrangedSubview(button)
         }
         filtersView.layoutIfNeeded()
+    }
+    
+    @objc func filterPressed(sender: UIButton) {
+        do {
+            processedImage = try imageProcessor.applyFilter(sender.currentTitle, to: originalImage, withPower: 1.0)
+            imageView.image = processedImage
+        } catch FilterError.noImage {
+            showError("No image!")
+        } catch FilterError.processingError {
+            showError("Error during processing!")
+        } catch let FilterError.unknowFilter(f) {
+            showError("Filter \(f) not found!")
+        } catch FilterError.ciFailed {
+            showError("Internal error!")
+        } catch {
+            showError("Unexpected error!")
+        }
     }
     
     @IBAction func toggleFilters(_ sender: UIButton) {
@@ -37,6 +63,13 @@ class ViewController: UIViewController {
                 sender.isSelected = false
             }
         }
+    }
+    
+    
+    func showError(_ error: String) {
+        let alert = UIAlertController(title: "Error", message: error, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
